268f21deea5115a5b61dbaff988f605c38b9c2be
HamidReza <delshad.swdn@gmail.com>
2024-02-05 02:49:04 +0330

    navbar, color setting added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "k-brown": "#926b48",
+        "k-lightBrown": "#c48d63",
+        "K-xlightBrown": "#daa676",
+        "k-lightBlue": "#0091c9",
+        "K-blue": "#0076af",
+        "k-nafti": "#00467d",
+        "k-darkBlue": "#00557d",
+      },
+      fontFamily: {
+        sans: ["Noto Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
