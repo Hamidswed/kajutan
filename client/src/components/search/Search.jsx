bf3d7518cf0ea716791418a1035cf89b0a5e1830
HamidReza <delshad.swdn@gmail.com>
2024-02-25 23:38:30 +0330

    sticky search bar added

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -2,26 +2,28 @@ import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/16/solid";
 
 export function Search({ searchItem, setSearchItem }) {
   return (
-    <div className="flex justify-between items-center mb-4 border-b border-k-xlightBrown p-2 text-k-xlightBrown lg:max-w-screen-lg">
-      <div className="w-full">
-        <input
-          type="text"
-          value={searchItem}
-          onChange={(e) => setSearchItem(e.target.value)}
-          placeholder="Sök t ex Calzone..."
-          className="bg-transparent outline-none placeholder-k-brown placeholder-opacity-50 w-full"
-        />
-      </div>
-      <button className="w-5 h-5">
-        {searchItem === "" ? (
-          <MagnifyingGlassIcon className="w-full text-k-xlightBrown hover:text-white transition-all duration-300 hover:scale-125" />
-        ) : (
-          <XMarkIcon
-            onClick={() => setSearchItem("")}
-            className="w-full text-k-xlightBrown hover:text-white transition-all duration-300 hover:scale-125"
+    <div className="sticky top-0 z-10 p-2 rounded-md backdrop-blur-md">
+      <div className="flex justify-between items-center mb-4 border-b border-k-xlightBrown p-2 text-k-xlightBrown lg:max-w-screen-lg">
+        <div className="w-full">
+          <input
+            type="text"
+            value={searchItem}
+            onChange={(e) => setSearchItem(e.target.value)}
+            placeholder="Sök t ex Calzone..."
+            className="bg-transparent outline-none placeholder-k-brown placeholder-opacity-50 w-full"
           />
-        )}
-      </button>
+        </div>
+        <button className="w-5 h-5">
+          {searchItem === "" ? (
+            <MagnifyingGlassIcon className="w-full text-k-xlightBrown hover:text-white transition-all duration-300 hover:scale-125" />
+          ) : (
+            <XMarkIcon
+              onClick={() => setSearchItem("")}
+              className="w-full text-k-xlightBrown hover:text-white transition-all duration-300 hover:scale-125"
+            />
+          )}
+        </button>
+      </div>
     </div>
   );
 }
