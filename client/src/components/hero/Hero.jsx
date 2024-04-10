87cefc5a144c600ef139254f244169045191977a
HamidReza <delshad.swdn@gmail.com>
2024-04-10 15:26:17 +0330

    some bugs in images fixed

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import Pizza from "../../assets/hero.png";
 import BgLargWp from "../../assets/background/bg-large.webp";
-import BgLarg from "../../assets/background/bg-large.png";
+import BgLarg from "../../assets/background/bg-large.jpg";
 import BgSmallWp from "../../assets/background/bg-small.webp";
-import BgSmall from "../../assets/background/bg-small.png";
+import BgSmall from "../../assets/background/bg-small.jpg";
 
 export function Hero() {
   return (
     <div className="w-full flex justify-center items-center mt-16">
       <div className="w-full relative flex justify-center">
         {/* <img className="translate-y-8" src={Pizza} alt="pizza" /> */}
-        <picture className="translate-y-[calc(20%+1rem)] md:translate-y-[calc(25%+2rem)]">
+        <picture className="translate-y-[calc(35%-10px)] md:translate-y-[calc(25%+2rem)] w-full">
           <source
             srcSet={BgLargWp}
             type="image/webp"
@@ -27,9 +27,9 @@ export function Hero() {
             type="image/jpg"
             media="(max-width: 768px)"
           />
-          <img alt="banner" src={BgLarg} />
+          <img alt="banner" src={BgLarg} className="w-full"/>
         </picture>
-        <div className="text-white flex flex-col items-center absolute z-10 top-10 min-[430px]:top-16 min-[460px]:top-20 md:top-8 lg:top-20">
+        <div className="text-white flex flex-col items-center absolute z-10 top-6 min-[430px]:top-16 min-[460px]:top-20 md:top-8 xl:top-24">
           <p className="text-k-lightBrown min-[520px]:text-lg md:text-2xl lg:text-4xl">
             Välkomna till
           </p>
@@ -37,7 +37,7 @@ export function Hero() {
             Kajutan
           </p>
         </div>
-        <div className="text-white absolute top-[calc(27%+2rem)] flex flex-col gap-y-3 xl:gap-y-6">
+        <div className="text-white absolute top-[calc(27%+2rem)] min-[520px]:top-[40%] flex flex-col gap-y-3 xl:gap-y-6">
           <p className="md:text-xl xl:text-3xl">Kolla på meny!</p>
           <button className=" border-2 border-k-lightBrown py-2 rounded-xl animate-pulse hover:bg-k-lightBrown xl:text-xl">
             <Link to="/menu">Meny</Link>
